56386ef5027da9ee7a7668e6a75602c02dcc873f
shambhavicoding <77612599+shambhavicoding@users.noreply.github.com>
2021-07-21 18:55:28 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA5xepbCzcLjDxwAojWyij31gWKgfxZVAs",
+    authDomain: "project-94-ebd6c.firebaseapp.com",
+    projectId: "project-94-ebd6c",
+    storageBucket: "project-94-ebd6c.appspot.com",
+    messagingSenderId: "135948924575",
+    appId: "1:135948924575:web:7282cd7619d07422b814aa"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
